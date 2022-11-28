5a27f7cc4326142d366acc495a741750cc7b51cb
Danting Huang <serena.huangdanting@gmail.com>
2022-11-28 01:03:32 -0500

    complete interviewing entry functionality

--- client/src/components/InterviewEntry.jsx
+++ client/src/components/InterviewEntry.jsx
@@ -1,17 +1,24 @@
-import React from "react";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faFaceLaugh, faHeartBroken } from "@fortawesome/free-solid-svg-icons";
-// import TimerTwoToneIcon from "@mui/icons-material/TimerTwoTone";
+import React, { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
-import AlarmIcon from '@mui/icons-material/Alarm';
+import AlarmIcon from "@mui/icons-material/Alarm";
 import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import MoodIcon from '@mui/icons-material/Mood';
+import SentimentVeryDissatisfiedIcon from '@mui/icons-material/SentimentVeryDissatisfied';
 import { yellow, blue, orange, red } from "@mui/material/colors";
 import { IconButton, Button, Modal, Box, Stack, Tooltip } from "@mui/material";
 
 function InterviewEntry(props) {
+  const [offerConfirm, setOfferConfirm] = useState(false);
+  const [rejPop, setRejPop] = useState(false);
+  const handleOfferConfirmClose = (e) => {
+    setOfferConfirm(false);
+  };
+  const handleRejClose = (e) => {
+    setRejPop(false);
+  };
   const interviewDate = new Date(props.interview.interview_date);
   const today = new Date();
   const calDays = Math.ceil((interviewDate - today) / (1000 * 60 * 60 * 24));
@@ -20,10 +27,10 @@ function InterviewEntry(props) {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 200,
+    width: 300,
     bgcolor: "background.paper",
     boxShadow: 24,
-    p: 3,
+    p: 6,
   };
 
   return (
@@ -72,45 +79,101 @@ function InterviewEntry(props) {
               >
                 Interview on{" "}
                 {new Date(props.interview.interview_date).toLocaleString()}
-              </Typography >
-              {calDays > 0
-              ?
-              <Typography
-              style={{
-                fontFamily: "Georgia",
-                fontSize: "0.8rem",
-                color: red[900],
-                fontStyle: "italic",
-              }}>(in {calDays} days)</Typography>
-              : calDays < 0
-              ? <Typography
-                style={{
-                  fontFamily: "Georgia",
-                  fontSize: "0.8rem",
-                  color: blue[900],
-                  fontStyle: "italic",
-                }}>({calDays} days ago)</Typography>
-              : <Typography
-              style={{
-                fontFamily: "Georgia",
-                fontSize: "1rem",
-                color: red[900],
-                fontStyle: "italic",
-              }}>(TODAY)</Typography>
-              }
-
+              </Typography>
+              {calDays > 0 ? (
+                <Typography
+                  style={{
+                    fontFamily: "Georgia",
+                    fontSize: "0.8rem",
+                    color: red[900],
+                    fontStyle: "italic",
+                  }}
+                >
+                  (in {calDays} days)
+                </Typography>
+              ) : calDays < 0 ? (
+                <Typography
+                  style={{
+                    fontFamily: "Georgia",
+                    fontSize: "0.8rem",
+                    color: blue[900],
+                    fontStyle: "italic",
+                  }}
+                >
+                  ({calDays} days ago)
+                </Typography>
+              ) : (
+                <Typography
+                  style={{
+                    fontFamily: "Georgia",
+                    fontSize: "1rem",
+                    color: red[900],
+                    fontStyle: "italic",
+                  }}
+                >
+                  (TODAY)
+                </Typography>
+              )}
             </Grid>
             <Stack direction="row" alignItems="center" spacing={8}>
-            <IconButton aria-label="thumbUp" size="large">
+              <Tooltip title="Marks as Offer Received">
+              <IconButton
+                aria-label="thumbUp"
+                size="large"
+                onClick={(e) => setOfferConfirm(true)}
+              >
                 <ThumbUpIcon />
               </IconButton>
-              <IconButton aria-label="thumbDown" size="large">
-                <ThumbDownIcon />
-              </IconButton>
+              </Tooltip>
+
+              <Tooltip title="Mark as Rejected">
+                <IconButton
+                  aria-label="thumbDown"
+                  size="large"
+                  onClick={(e) => setRejPop(true)}
+                >
+                  <ThumbDownIcon />
+                </IconButton>
+              </Tooltip>
+
             </Stack>
           </Grid>
         </Grid>
       </Grid>
+      {offerConfirm ? (
+        <Modal
+          open={offerConfirm}
+          onClose={handleOfferConfirmClose}
+          aria-labelledby="offer-confirmation-modal"
+        >
+          <Box sx={{...style, bgcolor: yellow[200]}}>
+            <Typography id="offer-confirm-modal-title" variant="h6" component="h2">
+              Received OFFER on this application?
+            </Typography>
+            <Stack direction="row" align="center" spacing={20}>
+              <Button id={`${props.interview._id}-offerBtn`} onClick={props.clickOfferBtn}> <MoodIcon /> YES!!!</Button>
+              <Button onClick={handleOfferConfirmClose}> Cancel</Button>
+            </Stack>
+          </Box>
+        </Modal>
+      ) : null}
+      {rejPop ? (
+        <Modal
+          open={rejPop}
+          onClose={handleRejClose}
+          aria-labelledby="rej-comfirmation-modal"
+        >
+          <Box sx={style}>
+            <Typography id="rej-modal-title" variant="h6" component="h2">
+              Mark this application as Rejected?
+            </Typography>
+            <Stack direction="row" align="center" spacing={20}>
+              <Button id={`${props.interview._id}-itw-rejBtn`} onClick={props.clickRejBtn}> <SentimentVeryDissatisfiedIcon /> YES</Button>
+              <Button onClick={handleRejClose}> Cancel</Button>
+            </Stack>
+          </Box>
+        </Modal>
+      ) : null}
     </Paper>
     // <div>
     //   <TimerTwoToneIcon n />
@@ -121,8 +184,8 @@ function InterviewEntry(props) {
     //   </span>{" "}
     //   <span className="time-stamp">(in {calDays} days)</span>
     //   <button
-    //     id={`${props.interview._id}-offerBtn`}
-    //     onClick={props.clickOfferBtn}
+        // id={`${props.interview._id}-offerBtn`}
+        // onClick={props.clickOfferBtn}
     //   >
     //     <FontAwesomeIcon icon={faFaceLaugh} />
     //     OFFER
